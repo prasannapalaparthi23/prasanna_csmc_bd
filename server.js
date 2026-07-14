57db7727082e4675990b2bc059e11ded36153df5
tammirsidevika <tammirsidevika05@gmail.com>
2026-07-14 09:27:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-
+const dns=require("dns")
+dns.setServers(["8.8.8.8","8.8.4.4"])
 dotenv.config();
 
 const connectDB = require("./config/db");
